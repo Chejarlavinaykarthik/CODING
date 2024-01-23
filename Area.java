74ad9bcd93f32f755ab6e3f4320cf009231c6495
Chejarlavinaykarthik <cvkarthikreddy99@gmail.com>
2024-01-23 19:17:21 +0530

    Hii

---
+++ Area.java
@@ -0,0 +1,19 @@
+class circle
+{
+	public static void main(String args[])
+	{
+		int length = 5;
+		int breadth = 6;
+		int radius = 2;
+		int A = length*breadth;
+		int P = 2*(length+breadth);
+		int C = 2*2*22/7;
+		double Ar = 3.14*radius*radius;
+		System.out.println(A);
+		System.out.println(P);
+		System.out.println(C);
+		System.out.println(Ar);
+
+	}
+}
+		
